c6339b3013599877a3ff1f55ae857330bbce9c62
Jason <enigsuss@gmail.com>
2024-07-25 17:05:22 +0900

    feat: 이제 마커를 클릭하면 주차장 정보가 표시됩니다.

--- src/pages/StadiumPage/components/PickerMapSection/components/ParkRealTimeSection/ParkRealTimeSection.jsx
+++ src/pages/StadiumPage/components/PickerMapSection/components/ParkRealTimeSection/ParkRealTimeSection.jsx
@@ -1,8 +1,38 @@
 import * as S from './ParkRealTimeSection.styled'
+import useGetParkInfo from '@/hooks/apis/useGetParkInfo.js'
 
 function ParkRealTimeSection({ markerInfoForPark }) {
   console.log('markerInfoForPark', markerInfoForPark) // 예시를 위한 console입니다. 작업 마치신 후 지워주세요
-  return <S.StateSection>공원 실시간 정보</S.StateSection>
+
+  const name = markerInfoForPark.address.PRKPLCENM
+  const location = markerInfoForPark.address.LNMADR
+  const openDay = markerInfoForPark.address.OPERDAY
+  const openTimeDefault = markerInfoForPark.address.WEEKDAYOPEROPENHHMM
+  const closeTimeDefault = markerInfoForPark.address.WEEKDAYOPERCOLSEHHMM.slice(
+    0,
+    -3
+  )
+  const openTimeSat = markerInfoForPark.address.SATOPEROPEROPENHHMM
+  const closeTimeSat = markerInfoForPark.address.SATOPERCLOSEHHMM
+  const openTimeHolliday = markerInfoForPark.address.HOLIDAYOPEROPENHHMM
+  const closeTimeHolliday = markerInfoForPark.address.HOLIDAYCLOSEOPENHHMM
+
+  return (
+    <S.StateSection>
+      <S.Name>{name} 주차장</S.Name>
+      <S.Location>{location}</S.Location>
+      <S.InfoContainer>
+        <S.Info>개장일 : {openDay}</S.Info>
+        <S.Info>
+          개장시간(평일) : {`${openTimeDefault} ~ ${closeTimeDefault}`}
+        </S.Info>
+        <S.Info>개장시간(토요일) : {`${openTimeSat} ~ ${closeTimeSat}`}</S.Info>
+        <S.Info>
+          개장시간(공휴일) : {`${openTimeHolliday} ~ ${closeTimeHolliday}`}
+        </S.Info>
+      </S.InfoContainer>
+    </S.StateSection>
+  )
 }
 
 export default ParkRealTimeSection
--- src/pages/StadiumPage/components/PickerMapSection/components/ParkRealTimeSection/ParkRealTimeSection.styled.js
+++ src/pages/StadiumPage/components/PickerMapSection/components/ParkRealTimeSection/ParkRealTimeSection.styled.js
@@ -3,9 +3,34 @@ import styled from 'styled-components'
 export const StateSection = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   width: 100%;
+  /* background-color: red; */
   border: 1px solid #e0e0e0;
   border-radius: 20px;
   padding: 20px;
   margin-top: 40px;
+`
+
+export const Name = styled.div`
+  font-size: 35px;
+  font-weight: 700;
+  margin-bottom: 10px;
+`
+export const Location = styled.div`
+  font-size: 25px;
+  margin-bottom: 15px;
+`
+
+export const Info = styled.div`
+  font-size: 20px;
+`
+
+export const InfoContainer = styled.div`
+  font-size: 20px;
+  width: 300px;
+  border: 1px solid gray;
+  border-radius: 10px;
+  padding: 10px;
+  text-align: left;
 `
