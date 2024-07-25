528a705b7bb9243b80fd4ad9a84ba1628abc2b64
Yejin0O0 <yyj11kr@naver.com>
2024-07-25 16:33:13 +0900

    feat: weather state 날라가는 문제 해결

--- src/pages/StadiumPage/components/WeatherSection/StadiumWeather.jsx
+++ src/pages/StadiumPage/components/WeatherSection/StadiumWeather.jsx
@@ -1,3 +1,4 @@
+import useWeather from '@/hooks/utils/useWeather'
 import * as S from '@/pages/StadiumPage/components/WeatherSection/WeatherSection.styled'
 import useSelector from '@/react-redux/hooks/useSelector.js'
 import getImageBySkyStatus from '@/utils/getImageBySkyStatus'
@@ -7,13 +8,14 @@ function StadiumWeather({ lat, lng }) {
   const latStr = Math.floor(lat).toString()
   const lngStr = Math.floor(lng).toString()
 
-  const path = useParams().stadiumName
+  const stadiumName = useParams().stadiumName
 
   const weatherData = useSelector(
-    state => state.weather.stadiumWeatherMap[path]
+    state => state.weather.stadiumWeatherMap[stadiumName]
   )
 
   const skySrc = getImageBySkyStatus(weatherData?.sky)
+  const { error } = useWeather(stadiumName, latStr, lngStr)
 
   return (
     <S.WeatherWrapper>
