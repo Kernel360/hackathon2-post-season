c46a64c86908349e5a3b22dae61d0385d8d62fb3
Yejin <78341274+Yejin0O0@users.noreply.github.com>
2024-07-25 17:09:49 +0900

    feat: weather state 날라가는 문제 해결 (#32)

--- src/hooks/utils/useWeather.js
+++ src/hooks/utils/useWeather.js
@@ -1,71 +1,96 @@
 /* eslint-disable no-inner-declarations */
-import axios from 'axios'
-import { useEffect, useState } from 'react'
 import useDispatch from '@/react-redux/hooks/useDispatch'
+import useSelector from '@/react-redux/hooks/useSelector'
 import { setWeatherActionCreator } from '@/redux/actions/weatherAction'
 import getCurrentDate from '@/utils/date/getCurrentDate.js'
 import getCurrentSharpTime from '@/utils/date/getCurrentSharpTime.js'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-const useWeather = (id, latitude, longitude) => {
+const fetchWeather = async (
+  latitude,
+  longitude,
+  isCurrentTimeRequired = true
+) => {
+  const serviceKey = `4ilvM/MZHjnpTB21ujv6m61TBG3kyYMnMQG+3w0AZ+Wan/+9cBoMsa4ho2BYjLPUDJWfL+zRJ6YrufBiA8JFbA==`
+  const pageNo = `1`
+  const numOfRows = `60`
+  const dataType = `json`
+  const baseDate = getCurrentDate()
+  const baseTime = getCurrentSharpTime(isCurrentTimeRequired)
+  const url = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst`
+
+  try {
+    // console.log('호출')
+    const response = await axios.get(url, {
+      params: {
+        serviceKey,
+        numOfRows,
+        pageNo,
+        dataType,
+        base_date: baseDate,
+        base_time: baseTime,
+        nx: latitude,
+        ny: longitude,
+      },
+    })
+    const { data } = response
+    // console.log(data)
+    if (!data.response.body) {
+      // console.log(data)
+      if (isCurrentTimeRequired) {
+        await fetchWeather(false)
+      } else {
+        throw new Error('유효한 데이터를 받을 수 없습니다.')
+      }
+    } else {
+      function getWeatherByCategory(category) {
+        const weatherItems = data.response.body.items.item.filter(
+          item => item.category === category
+        )
+        return weatherItems
+      }
+
+      const skyData = getWeatherByCategory('SKY')[0].fcstValue
+      const windData = getWeatherByCategory('WSD')[0].fcstValue
+      const temperatureData = getWeatherByCategory('T1H')[0].fcstValue
+
+      const result = {
+        skyData,
+        windData,
+        temperatureData,
+      }
+      return result
+    }
+  } catch (e) {
+    // setError(e)
+    // console.log(data)
+    console.error('fetch 과정 중 에러 발생', e)
+  }
+}
+
+const useWeather = (stadiumName, latitude, longitude) => {
   const [error, setError] = useState(null)
 
   const dispatch = useDispatch()
 
-  const fetchWeather = async (isCurrentTimeRequired = true) => {
-    const serviceKey = `F2tl5d/WnzODJeNwAy+pHFW5WgA57X7fcaTNPse/rEUfd1StDJWqJglkWMjT04qOa+YCyp0plNd+rEJCFgFsFw==`
-    const pageNo = `1`
-    const numOfRows = `60`
-    const dataType = `json`
-    const baseDate = getCurrentDate()
-    const baseTime = getCurrentSharpTime(isCurrentTimeRequired)
-    const url = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst`
-
-    try {
-      const response = await axios.get(url, {
-        params: {
-          serviceKey,
-          numOfRows,
-          pageNo,
-          dataType,
-          base_date: baseDate,
-          base_time: baseTime,
-          nx: latitude,
-          ny: longitude,
-        },
-      })
-      const { data } = response
-      if (!data.response.body) {
-        if (isCurrentTimeRequired) {
-          await fetchWeather(false)
-        } else {
-          throw new Error('유효한 데이터를 받을 수 없습니다.')
-        }
-      } else {
-        function getWeatherByCategory(category) {
-          const weatherItems = data.response.body.items.item.filter(
-            item => item.category === category
-          )
-          return weatherItems
-        }
-
-        const skyData = getWeatherByCategory('SKY')[0].fcstValue
-        const windData = getWeatherByCategory('WSD')[0].fcstValue
-        const temperatureData = getWeatherByCategory('T1H')[0].fcstValue
-
-        // console.log(sky, temperature)
-
-        dispatch(
-          setWeatherActionCreator(id, skyData, windData, temperatureData)
-        )
-      }
-    } catch (e) {
-      setError(e)
-      console.error('fetch 과정 중 에러 발생', e)
-    }
-  }
+  const weatherData = useSelector(
+    state => state.weather.stadiumWeatherMap[stadiumName]
+  )
 
   useEffect(() => {
-    fetchWeather()
+    async function updateWeather() {
+      const { skyData, windData, temperatureData } = await fetchWeather(
+        latitude,
+        longitude
+      )
+      dispatch(
+        setWeatherActionCreator(stadiumName, skyData, windData, temperatureData)
+      )
+    }
+    if (weatherData === undefined) {
+      updateWeather()
+    }
   }, [latitude, longitude])
 
   return { error }
